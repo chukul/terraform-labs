70a3ac9ef62f5bcc5974e7f1b0041956da62ae42
Chuchai Kultanahiran <Chuchai.Kultanahiran@neosit.com>
2019-06-18 10:52:27 +0700

    tf-az

--- nsgs.tf
+++ nsgs.tf
@@ -26,6 +26,21 @@ resource "azurerm_network_security_rule" "AllowSSH" {
     source_address_prefix       = "*"
 }
 
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowSSH"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+
+    priority                    = 1110
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+
 resource "azurerm_network_security_rule" "AllowHTTP" {
     name = "AllowHTTP"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
@@ -86,6 +101,24 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = 22
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_security_group" "nic_windows" {
+   name = "NIC_Windows"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name                       = "Allow_RDP"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 3389
         source_address_prefix      = "*"
         destination_address_prefix = "*"
   }
