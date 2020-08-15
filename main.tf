99e7d04ea479c0128b501e6e0857ef59938d8bb6
chukul <43925877+chukul@users.noreply.github.com>
2020-08-16 01:24:42 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,3 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
 }
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.123.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
-}
