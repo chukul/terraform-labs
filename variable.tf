71d76b86414667e45025640ee93578ae8b012dd0
chukul <43925877+chukul@users.noreply.github.com>
2020-08-16 01:34:07 +0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,21 @@ variable "secret_access_key" {
   type        = string
   description = "secret_access_key"
 }
+
+variable "aws_region" {
+	default = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-east-1a", "us-east-1b"]
+}
