01af85aa378989934cc94aae22307e2764ac2c3f
chukul <43925877+chukul@users.noreply.github.com>
2020-08-16 01:16:01 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 3.0"
+  region  = "ap-southeast-1"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.123.0.0/16"
   instance_tenancy = "default"
