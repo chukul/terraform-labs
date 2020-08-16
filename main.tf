28a9ca7fa207b5192e99622181b6daf71becc71c
chukul <43925877+chukul@users.noreply.github.com>
2020-08-16 12:47:33 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,29 +13,29 @@ resource "aws_vpc" "terra_vpc" {
 
 # Internet Gateway
 resource "aws_internet_gateway" "terra_igw" {
-  vpc_id = "${aws_vpc.terra_vpc.id}"
+  vpc_id = aws_vpc.terra_vpc.id
 }
 
 # Subnets : public
 resource "aws_subnet" "public" {
-  count = "${length(var.subnets_cidr)}"
-  vpc_id = "${aws_vpc.terra_vpc.id}"
-  cidr_block = "${element(var.subnets_cidr,count.index)}"
-  availability_zone = "${element(var.azs,count.index)}"
+  count = length(var.subnets_cidr)
+  vpc_id = aws_vpc.terra_vpc.id
+  cidr_block = element(var.subnets_cidr,count.index)
+  availability_zone = element(var.azs,count.index)
 }
 
 # Route table: attach Internet Gateway 
 resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.terra_vpc.id}"
+  vpc_id = aws_vpc.terra_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.terra_igw.id}"
+    gateway_id = aws_internet_gateway.terra_igw.id
   }
 }
 
 # Route table association with public subnets
 resource "aws_route_table_association" "a" {
-  count = "${length(var.subnets_cidr)}"
-  subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
-  route_table_id = "${aws_route_table.public_rt.id}"
+  count = length(var.subnets_cidr)
+  subnet_id      = element(aws_subnet.public.*.id,count.index)
+  route_table_id = aws_route_table.public_rt.id
 }
