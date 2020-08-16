2ab0ee6847dbe476d8b5463ae01eb564a453c50e
chukul <43925877+chukul@users.noreply.github.com>
2020-08-16 16:50:03 +0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -21,6 +21,11 @@ variable "subnets_cidr" {
 	default = ["10.20.1.0/24", "10.20.2.0/24"]
 }
 
+variable "private_ips" {
+	type = list(string)
+	default = ["10.20.1.10", "10.20.2.10"]
+}
+
 variable "azs" {
 	type = list(string)
 	default = ["ap-southeast-1a", "ap-southeast-1b"]
