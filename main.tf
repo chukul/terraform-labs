54e8deea0dd5787b701acef43a0e87bb138a8b74
chukul <pong2day@gmail.com>
2020-08-21 17:16:32 +0700

    updated

--- main.tf
+++ main.tf
@@ -35,3 +35,27 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks      = ["172.25.0.0/16","172.24.0.0/16","10.102.0.0/16","206.1.1.0/24","206.1.2.0/24","192.168.0.0/16"]
+  ingress_rules            = ["all-all"]
+  # ingress_with_cidr_blocks = [
+  #   {
+  #     from_port   = 8080
+  #     to_port     = 8090
+  #     protocol    = "tcp"
+  #     description = "User-service ports"
+  #     cidr_blocks = "10.10.0.0/16"
+  #   },
+  #   {
+  #     rule        = "postgresql-tcp"
+  #     cidr_blocks = "0.0.0.0/0"
+  #   },
+  # ]
+}
