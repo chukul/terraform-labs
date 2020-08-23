1d1aa86a7df5bea0d12cca0dc7cafc507166e860
chukul <pong2day@gmail.com>
2020-08-23 15:09:29 +0700

    updated

--- main.tf
+++ main.tf
@@ -17,49 +17,49 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
 }
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
 
-  name = "tli-aws-nonprod-ifrs-dev"
-  cidr = "172.25.131.0/26"
+#   name = "tli-aws-nonprod-ifrs-dev"
+#   cidr = "172.25.131.0/26"
 
-  azs             = ["ap-southeast-1a"]
-  private_subnets = ["172.25.131.0/27", "172.25.131.32/27"]
-  #public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#   azs             = ["ap-southeast-1a"]
+#   private_subnets = ["172.25.131.0/27", "172.25.131.32/27"]
+#   #public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+#   enable_nat_gateway = false
+#   enable_vpn_gateway = false
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Terraform = "true"
+#     Environment = "dev"
+#   }
+# }
 
-module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+# module "vote_service_sg" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = "${module.vpc.vpc_id}"
+#   name        = "user-service"
+#   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+#   vpc_id      = "${module.vpc.vpc_id}"
 
-  ingress_cidr_blocks      = ["172.25.0.0/16","172.24.0.0/16","10.102.0.0/16","206.1.1.0/24","206.1.2.0/24","192.168.0.0/16"]
-  ingress_rules            = ["all-all"]
-  egress_rules             = ["all-all"]
-  # ingress_with_cidr_blocks = [
-  #   {
-  #     from_port   = 8080
-  #     to_port     = 8090
-  #     protocol    = "tcp"
-  #     description = "User-service ports"
-  #     cidr_blocks = "10.10.0.0/16"
-  #   },
-  #   {
-  #     rule        = "postgresql-tcp"
-  #     cidr_blocks = "0.0.0.0/0"
-  #   },
-  # ]
-}
+#   ingress_cidr_blocks      = ["172.25.0.0/16","172.24.0.0/16","10.102.0.0/16","206.1.1.0/24","206.1.2.0/24","192.168.0.0/16"]
+#   ingress_rules            = ["all-all"]
+#   egress_rules             = ["all-all"]
+#   # ingress_with_cidr_blocks = [
+#   #   {
+#   #     from_port   = 8080
+#   #     to_port     = 8090
+#   #     protocol    = "tcp"
+#   #     description = "User-service ports"
+#   #     cidr_blocks = "10.10.0.0/16"
+#   #   },
+#   #   {
+#   #     rule        = "postgresql-tcp"
+#   #     cidr_blocks = "0.0.0.0/0"
+#   #   },
+#   # ]
+# }
 
 # module "ec2_cluster" {
 #   source                 = "terraform-aws-modules/ec2-instance/aws"
