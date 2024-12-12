d1c74c478e5f37710009aabe8ec2a0fc7fa820d5
Chuchai Kultanahiran <chuchai.k@cimbthai.com>
2024-12-12 15:44:28 +0700

    test code

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-resource "null_resource" "example1" {}
+provider "aws" {
+  region = "ap-southeast-1"  # Replace with your desired region
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-southeast-1"  # Replace with your desired availability zone
+  size              = 10            # Size in GB
+  type              = "gp3"         # General Purpose SSD (gp2)
+
+  tags = {
+    Name = "example-ebs-volume"
+  }
+}
