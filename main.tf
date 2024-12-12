16ee953305c3701672e805dc73fc082f3066450f
chukul <43925877+chukul@users.noreply.github.com>
2024-12-12 11:09:15 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,4 @@
-resource "null_resource" "example" {}
+resource "null_resource" "example1" {}
+resource "null_resource" "example2" {}
+resource "null_resource" "example3" {}
+resource "null_resource" "example4" {}
