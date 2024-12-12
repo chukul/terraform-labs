b22e641c398ae082caf54af533d5908a9d792c86
Chuchai Kultanahiran <chuchai.k@cimbthai.com>
2024-12-12 11:48:02 +0700

    remove 2 null

--- main.tf
+++ main.tf
@@ -2,5 +2,3 @@ resource "null_resource" "example1" {}
 resource "null_resource" "example2" {}
 resource "null_resource" "example3" {}
 resource "null_resource" "example4" {}
-resource "null_resource" "example5" {}
-resource "null_resource" "example6" {}
