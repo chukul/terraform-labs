35f2a03d9b7121ee63d723e75739df8d6691ad5a
Chuchai Kultanahiran <chuchai.k@cimbthai.com>
2024-12-13 13:57:45 +0700

    add ebs

--- development/main.tf
+++ development/main.tf
@@ -26,3 +26,19 @@ resource "aws_ebs_volume" "example" {
     ProjectName = "Pool"
   }
 }
+
+resource "aws_ebs_volume" "example2" {
+  availability_zone = "ap-southeast-1a" # Replace with your desired availability zone
+  size              = 10               # Size in GB
+  type              = "gp3"            # General Purpose SSD (gp2)
+
+  tags = {
+    Name = "dev2-example-ebs-volume"
+    map-migrated = "migJL95TU4VS9"
+    CostCenter = "Engineering"
+    Env = "CCoE"
+    Owner = "Sre-Team"
+    EmailCreator = "chuchai.k@cimbthai.com"
+    ProjectName = "Pool"
+  }
+}
