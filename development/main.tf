e94ae7931864de83f861117e703c5a614982e9bb
Chuchai Kultanahiran <chuchai.k@cimbthai.com>
2024-12-13 13:54:26 +0700

    fix

--- development/main.tf
+++ development/main.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-atlantis"  # Replace with your bucket name
+    key            = "terraform.tfstate"         # Path to the state file in the bucket
     region         = "ap-southeast-1"                 # Replace with your bucket region
     encrypt        = true                        # Enable encryption
   }
-} 
+}
 
 provider "aws" {
   region = "ap-southeast-1" # Replace with your desired region
