954ccab142cdcecf2e6e8c485f290c7cf2fa1aab
Chuchai Kultanahiran <chuchai.k@cimbthai.com>
2024-12-13 14:10:52 +0700

    destroy

--- development/main.tf
+++ development/main.tf
@@ -10,35 +10,3 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1" # Replace with your desired region
 }
-
-resource "aws_ebs_volume" "example" {
-  availability_zone = "ap-southeast-1a" # Replace with your desired availability zone
-  size              = 10               # Size in GB
-  type              = "gp3"            # General Purpose SSD (gp2)
-
-  tags = {
-    Name = "dev-example-ebs-volume"
-    map-migrated = "migJL95TU4VS9"
-    CostCenter = "Engineering"
-    Env = "CCoE"
-    Owner = "Sre-Team"
-    EmailCreator = "chuchai.k@cimbthai.com"
-    ProjectName = "Pool"
-  }
-}
-
-resource "aws_ebs_volume" "example2" {
-  availability_zone = "ap-southeast-1a" # Replace with your desired availability zone
-  size              = 15               # Size in GB
-  type              = "gp3"            # General Purpose SSD (gp2)
-
-  tags = {
-    Name = "dev2-example-ebs-volume"
-    map-migrated = "migJL95TU4VS9"
-    CostCenter = "Engineering"
-    Env = "CCoE"
-    Owner = "Sre-Team"
-    EmailCreator = "chuchai.k@cimbthai.com"
-    ProjectName = "Pool"
-  }
-}
