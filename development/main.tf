ae22bbf8d340e16d64e9b872a8a7bb378532e154
chukul <43925877+chukul@users.noreply.github.com>
2024-12-30 14:35:19 +0700

    Update main.tf

--- development/main.tf
+++ development/main.tf
@@ -26,3 +26,18 @@ resource "aws_ebs_volume" "example" {
     ProjectName = "Pool"
   }
 }
+resource "aws_ebs_volume" "example2" {
+  availability_zone = "ap-southeast-1a" # Replace with your desired availability zone
+  size              = 10               # Size in GB
+  type              = "gp3"            # General Purpose SSD (gp2)
+
+  tags = {
+    Name = "dev-example-ebs-volume2"
+    map-migrated = "migJL95TU4VS9"
+    CostCenter = "Engineering"
+    Env = "CCoE"
+    Owner = "Sre-Team"
+    EmailCreator = "chuchai.k@cimbthai.com"
+    ProjectName = "Pool"
+  }
+}
