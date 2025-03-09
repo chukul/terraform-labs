a2af0c633c86b673c736bd9cd9ffd409b12ee0ed
chukul <43925877+chukul@users.noreply.github.com>
2025-03-09 17:22:39 +0700

    Update main.tf

--- development/main.tf
+++ development/main.tf
@@ -12,3 +12,18 @@ provider "aws" {
 }
 
 # add atlantis comment to dev
+resource "aws_ebs_volume" "example" {
+   availability_zone = "ap-southeast-1a" # Replace with your desired availability zone
+   size              = 10               # Size in GB
+   type              = "gp3"            # General Purpose SSD (gp2)
+ 
+   tags = {
+     Name = "dev-example-ebs-volume"
+     map-migrated = "migJL95TU4VS9"
+     CostCenter = "Engineering"
+     Env = "CCoE"
+     Owner = "Sre-Team"
+     EmailCreator = "chuchai.k@cimbthai.com"
+     ProjectName = "Pool"
+   }
+ }
