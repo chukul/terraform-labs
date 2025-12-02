8fccc30f26118758fcce1802b47d6a6130bb17fa
Chuchai Kultanahiran <5358609@HL9CP4TGR7.local>
2025-12-02 23:56:14 +0700

    Add Atlantis demo resource and output

--- development/main.tf
+++ development/main.tf
@@ -8,6 +8,17 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
+resource "null_resource" "atlantis_demo" {
+  triggers = {
+    timestamp = "2025-12-02T23:55:00Z"
+    message   = "Testing Atlantis automation"
+  }
+}
+
 output "server_name" {
   value = "${random_pet.server.id}-${random_string.suffix.result}"
+}
+
+output "atlantis_status" {
+  value = "Atlantis is working!"
 }
